3f694e110c325e985499c13c8000897b4dc16d7c
pythoners620 <mohssinlkllay6@gmail.com>
2025-02-16 18:42:56 +0100

    Update git_repo2.py

--- git_repo2.py
+++ git_repo2.py
@@ -1,1 +1,10 @@
-print("Hello again!")
+print("Welcome to place the rabbit")
+field = [ ["🌿", "🌿", "🌿"], ["🌿", "🌿", "🌿"], ["🌿", "🌿", "🌿"] ]
+print(f"{field[0]} \n{field[1]} \n{field[2]} ")
+print("\nWere should the rabbit go? 🐇")
+position = input("Please choose a row and a column:")
+row = int(position[0])
+column = int(position[1])
+field[row-1][column-1] = "🐇"
+print("\nSuccess...\n")
+print(f"{field[0]} \n{field[1]} \n{field[2]} ")
