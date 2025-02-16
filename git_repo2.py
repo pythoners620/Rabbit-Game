ba0bdb1694403ffef63f0ba093972ecc957f4618
pythoners620 <mohssinlkllay6@gmail.com>
2025-02-16 18:45:45 +0100

    Update git_repo2.py

--- git_repo2.py
+++ git_repo2.py
@@ -8,3 +8,6 @@ column = int(position[1])
 field[row-1][column-1] = "🐇"
 print("\nSuccess...\n")
 print(f"{field[0]} \n{field[1]} \n{field[2]} ")
+
+
+# We have a simple project in Python. The project is a game about a rabbit that wants to go to the food. We have three rows and three columns. The first row has three columns and the second row has three columns. It also asks you to affect the row number and the column number.
